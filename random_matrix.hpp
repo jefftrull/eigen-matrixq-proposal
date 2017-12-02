c186e1be6a81bb64b2d5fc755d29e749afe19511
Jeff Trull <jeff.trull@ciere.com>
2017-12-02 11:03:10 -0800

    Add missing file

---
+++ random_matrix.hpp
@@ -0,0 +1,50 @@
+// shared methods for generating random sparse matrices
+
+// Copyright (C) 2017 Jeffrey E. Trull <edaskel@att.net>
+
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef RANDOM_MATRIX_HPP
+#define RANDOM_MATRIX_HPP
+
+#include <random>
+#include <Eigen/Sparse>
+
+template<typename Float>
+Eigen::SparseMatrix<Float>
+RandomMatrixOfSize(std::default_random_engine & gen,
+             Eigen::Index rows,
+             Eigen::Index cols,
+             float density) {
+    using namespace Eigen;
+    std::vector<Triplet<Float>> tripletList;
+    std::uniform_real_distribution<Float> dist(0.0,1.0);
+    for (Index i = 0; i < rows; i++) {
+        for (Index j = 0; j < cols; j++) {
+            // get a random number between 0 and 1
+            if (dist(gen) < density) {
+                // populate this entry
+                tripletList.emplace_back(i, j, 10 * dist(gen));
+            }
+        }
+    }
+    SparseMatrix<Float> mat(rows, cols);
+    mat.setFromTriplets(tripletList.begin(), tripletList.end());
+    return mat;
+}
+
+template<typename Float>
+Eigen::SparseMatrix<Float>
+RandomMatrix(std::default_random_engine & gen,
+             Eigen::Index max_dim,
+             float density) {
+    using namespace Eigen;
+    std::uniform_int_distribution<Eigen::Index> dim(1, max_dim);
+    Index x = dim(gen);
+    Index y = dim(gen);
+    return RandomMatrixOfSize<Float>(gen, x, y, density);
+}
+
+#endif // RANDOM_MATRIX_HPP
